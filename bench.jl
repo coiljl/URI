0bdc29beb5ea6fdab56d55a8d325e2374064aafd
Jake Rosoman <jkroso@gmail.com>
2016-01-05 15:06:55 +1300

    update benchmark

--- bench.jl
+++ bench.jl
@@ -1,10 +1,10 @@
+@require "github.com/johnmyleswhite/Benchmark.jl" compare
+@require "github.com/JuliaWeb/URIParser.jl" URI => A
 @require "." URI => B
-import URIParser.URI
-using Benchmark
 
 const uri = "http://test.com/a#frag"
 
 compare([
-  JuliaWeb() = URI(uri),
+  JuliaWeb() = A(uri),
   coiljl() = B(uri),
 ], 100) |> println
